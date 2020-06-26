3eeace6e00c34f434cc9ff3ec326e7c3c34b620f
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-06-26 19:26:29 +0800

    Added PseudoInverseTest file
    Pseudo inverse test with 4 horizontal thrusters

---
+++ pseudoInvTest.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+
+sin = math.sin(45)
+
+T = np.array((sin, sin, -sin, -sin, sin, -sin, sin, -sin, 1, -1, 1, -1))
+T = T.reshape(3,4)
+print(T)
+
+Tinv = np.linalg.pinv(T)
+print(Tinv)
+
+R = np.array((1, 0, 0))
+R = R.reshape(3,1)
+print(R)
+
+x = Tinv.dot(R)
+print(x)
+check = x.dot(Tinv)
